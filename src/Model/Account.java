9484e9bc1fb8adb57f81ebdb5034062e0fd7c964
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-04 12:06:28 +0100

    feat: create model account

---
+++ src/Model/Account.java
@@ -0,0 +1,61 @@
+package Model;
+
+import java.util.Objects;
+
+public class Account {
+    private String accountId;
+    private int balance ;
+    private String currency;
+
+    public Account(String accountId, int balance, String currency) {
+        this.accountId = accountId;
+        this.balance = balance;
+        this.currency = currency;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Account account = (Account) object;
+        return balance == account.balance && Objects.equals(accountId, account.accountId) && Objects.equals(currency, account.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, balance, currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId='" + accountId + '\'' +
+                ", balance=" + balance +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
