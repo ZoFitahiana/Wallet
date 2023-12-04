be6b2352bf028fe59aa9b0185ffb0e15685d9e24
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-04 16:31:03 +0100

    fix : update main to run test application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import Test.TestOfAccount;
+import Test.TestOfCurrency;
+import Test.TestOfTransaction;
+
 public class  Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Test of account :
+        TestOfAccount.TestAccount();
+
+        //Test of transaction :
+        TestOfTransaction.TestTransaction();
+
+        //Test of currency :
+        TestOfCurrency.TestCurrency();
+
     }
 }
