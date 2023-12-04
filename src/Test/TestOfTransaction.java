41a22b77ed76a4cd3b781e3f680b1df74779bbfa
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-04 16:05:31 +0100

    feat : Create test for transaction

---
+++ src/Test/TestOfTransaction.java
@@ -0,0 +1,23 @@
+package Test;
+
+import Model.Transaction;
+import Repository.TransactionCrudOperation;
+
+public class TestOfTransaction {
+public static  void TestTransaction(){
+
+    TransactionCrudOperation transactions = new TransactionCrudOperation();
+
+    // save transaction
+    Transaction transaction = new Transaction("txn004",60,"Purchase of goods","123456");
+    transactions.save(transaction);
+
+    // update transaction
+    Transaction SetTransaction =  new Transaction("txn004",100,"Salary deposit","123456");
+    transactions.update(SetTransaction);
+
+    // Find all transaction
+    transactions.findAll();
+
+}
+}
