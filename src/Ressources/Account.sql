9c84feb33b90c7315c3a26140fa8183caf2e5829
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-04 16:34:47 +0100

    fix: Update table acount

--- src/Ressources/Account.sql
+++ src/Ressources/Account.sql
@@ -1,19 +1,19 @@
 -- create table account
 CREATE TABLE IF NOT EXISTS account (
     accountId VARCHAR(255) PRIMARY KEY,
-    balance NUMERIC,
+    balance int,
     currencyId VARCHAR(255) REFERENCES currency(currencyId)
 );
 
 -- insert for account
 INSERT INTO account (accountId, balance, currencyId)
-VALUES ('123456', 1000.00, 'USD')
+VALUES ('123456', 1000, 'USD')
 ON CONFLICT (accountId) DO NOTHING;
 
 INSERT INTO account (accountId, balance, currencyId)
-VALUES ('789012', 500.00, 'EUR')
+VALUES ('789012', 500, 'EUR')
 ON CONFLICT (accountId) DO NOTHING;
 
 INSERT INTO account (accountId, balance, currencyId)
-VALUES ('345678', 750.50, 'GBP')
+VALUES ('345678', 750, 'GBP')
 ON CONFLICT (accountId) DO NOTHING;
