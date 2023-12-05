ca65db345147eb1475d49433f270155eaa2f0d5d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-05 09:03:10 +0100

    fix: Update test of account

--- src/test/TestOfAccount.java
+++ src/test/TestOfAccount.java
@@ -3,16 +3,20 @@ package test;
 import model.Account;
 import repository.AccountCrudOperation;
 
+import java.math.BigDecimal;
+
 public class TestOfAccount {
     public static void  TestAccount(){
         AccountCrudOperation accounts = new AccountCrudOperation();
 
         //Save account
-        Account account = new Account("123456UIO",3000,"USD");
+        BigDecimal balance = new BigDecimal("123.456");
+        Account account = new Account("123456UIO",balance,"USD");
         accounts.save(account);
 
         //Update account
-        Account SetAccount = new Account("123456UIO",3030,"EUR");
+        BigDecimal newBalance = new BigDecimal("200.00");
+        Account SetAccount = new Account("123456UIO",newBalance,"EUR");
         accounts.update(SetAccount);
 
         //Find all account
