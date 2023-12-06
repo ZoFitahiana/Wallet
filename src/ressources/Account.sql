2d2089e65897fc257351d8a0fac9dccb433fcbe8
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-06 19:10:02 +0100

    fix : Update table account

--- src/ressources/Account.sql
+++ src/ressources/Account.sql
@@ -4,19 +4,21 @@ CREATE TABLE IF NOT EXISTS account (
     balance NUMERIC,
     lastUpdate TIMESTAMP,
     currencyId VARCHAR(255) REFERENCES currency(currencyId),
-    transactionId VARCHAR(255) REFERENCES transaction(transactionId),
     type VARCHAR(50) CHECK (type IN ('Bank', 'Cash', 'Mobile Money'))
 );
 
 -- Insert data into account table
-INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, transactionId, type)
+INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, type)
 VALUES
-('ACC001', 'John Doe', 350.25, '2023-12-06 12:30:00', 'USD', 'TXN001', 'Bank');
+('ACC001', 'John Doe', 350.25, '2023-12-06 12:30:00', 'USD', 'Bank');
+ON CONFLICT (accountId) DO NOTHING;
 
-INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, transactionId, type)
+INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, type)
 VALUES
-('ACC002', 'Jane Smith', 120.75, '2023-12-06 12:40:00', 'EUR', 'TXN003', 'Cash');
+('ACC002', 'Jane Smith', 120.75, '2023-12-06 12:40:00', 'EUR', 'Cash');
+ON CONFLICT (accountId) DO NOTHING;
 
-INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, transactionId, type)
+INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, type)
 VALUES
-('ACC003', 'Alice Johnson', 500.00, '2023-12-06 13:30:00', 'GBP', 'TXN004', 'Mobile Money');
+('ACC003', 'Alice Johnson', 500.00, '2023-12-06 13:30:00', 'GBP', 'Mobile Money');
+ON CONFLICT (accountId) DO NOTHING;
