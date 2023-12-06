5bd3ddd7fdd4e40e8852984d649233a4a778c590
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-06 19:14:44 +0100

    fix: Update test of account and transaction

--- src/test/TestOfTransaction.java
+++ src/test/TestOfTransaction.java
@@ -19,7 +19,7 @@ public static  void TestTransaction(){
     System.out.println("Save transaction : ");
     LocalDateTime date = LocalDateTime.now();
     BigDecimal amount = new BigDecimal("100.45");
-    Transaction transaction = new Transaction("TXN001", amount, "Dinner", "CREDIT",date);
+    Transaction transaction = new Transaction("TXN001", amount, "Dinner", "CREDIT",date,"ACC001");
     transactions.save(transaction);
 
     // Find transaction by id :
@@ -29,15 +29,15 @@ public static  void TestTransaction(){
     // Test save list of transaction :
     System.out.println("Save list of transaction :");
     List<Transaction> ListOfTransaction = new ArrayList<>();
-    Transaction transaction1 = new Transaction("TXN004", amount, "Groceries", "DEBIT",date);
-    Transaction transaction2 = new Transaction("TXN003", amount, "Dinner", "DEBIT",date);
+    Transaction transaction1 = new Transaction("TXN004", amount, "Groceries", "DEBIT",date,"ACC001");
+    Transaction transaction2 = new Transaction("TXN003", amount, "Dinner", "DEBIT",date,"ACC003");
     ListOfTransaction.add(transaction1);
     ListOfTransaction.add(transaction2);
     transactions.saveAll(ListOfTransaction);
 
     // Test update Transaction  :
     System.out.println("Update transaction :");
-    Transaction transaction3 = new Transaction("TXN004", amount, "Bonus", "CREDIT",date);
+    Transaction transaction3 = new Transaction("TXN004", amount, "Bonus", "CREDIT",date,"ACC003");
     transactions.update(transaction3);
 
     // Test Find all :
