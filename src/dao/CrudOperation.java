399cc50a75e0a3dbad1e2a8713d75e41c72f558f
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-06 14:17:31 +0100

    fix: update CRUD operation

--- src/dao/CrudOperation.java
+++ src/dao/CrudOperation.java
@@ -3,7 +3,9 @@ package dao;
 import java.util.List;
 
 public interface CrudOperation<T> {
+    T findById(T id);
     List<T> findAll();
     T save(T toSave);
+    List<T> saveAll(List<T> tosave);
     T update(T toUpdate);
 }
