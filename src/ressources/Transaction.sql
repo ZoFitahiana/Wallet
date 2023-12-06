505f423bd3931f6340a4eebaa034106f8a278689
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-06 13:10:29 +0100

    feat: Update table transaction

--- src/ressources/Transaction.sql
+++ src/ressources/Transaction.sql
@@ -1,20 +1,24 @@
 -- create table transaction
 CREATE TABLE IF NOT EXISTS transaction (
     transactionId VARCHAR(255) PRIMARY KEY,
-    amount int,
-    description VARCHAR(255),
-    accountId VARCHAR(255) REFERENCES account(accountId)
+    amount NUMERIC NOT NULL,
+    label VARCHAR(255) NOT NULL,
+    type VARCHAR(10) NOT NULL CHECK (type IN ('DEBIT', 'CREDIT')),
+    date TIMESTAMP NOT NULL
 );
 
--- insert for transaction
-INSERT INTO transaction (transactionId, amount, description, accountId)
-VALUES ('txn001', 50, 'Purchase of goods', '123456')
+
+-- Insert  transaction
+INSERT INTO transaction  (transactionId, amount, label, type, date)
+VALUES ('TXN001', 100.50, 'Groceries', 'DEBIT', '2023-12-06 12:30:00')
 ON CONFLICT (transactionId) DO NOTHING;
 
-INSERT INTO transaction (transactionId, amount, description, accountId)
-VALUES ('txn002', -20, 'Refund for returned item', '789012')
+
+INSERT INTO transaction (transactionId, amount, label, type, date)
+VALUES ('TXN002', 50.25, 'Dinner', 'DEBIT', '2023-12-06 18:45:00')
 ON CONFLICT (transactionId) DO NOTHING;
 
-INSERT INTO transaction (transactionId, amount, description, accountId)
-VALUES ('txn003', 100.00, 'Salary deposit', '345678')
+
+INSERT INTO transaction (transactionId, amount, label, type, date)
+VALUES ('TXN003', 200.00, 'Salary', 'CREDIT', '2023-12-07 09:00:00')
 ON CONFLICT (transactionId) DO NOTHING;
