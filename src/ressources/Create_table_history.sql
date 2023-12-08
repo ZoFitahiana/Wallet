57877ffd303f0641d2dd2bf0dd9028845fd4da2d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-08 15:22:08 +0100

    feat : Create table history for account

--- src/ressources/Create_table_history.sql
+++
@@ -1,8 +0,0 @@
--- create table for history balance of account :
-
-CREATE TABLE IF NOT EXISTS history  (
-    historyId serial primary key  not null ,
-    transactionId varchar(200) not null ,
-    accountId varchar(200) not null ,
-    balance numeric
-);
---
+++ src/ressources/Create_table_history4_Account.sql
@@ -0,0 +1,16 @@
+-- Création de la séquence
+CREATE SEQUENCE history_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO CYCLE;
+
+-- Création de la table history avec utilisation de la séquence
+CREATE TABLE IF NOT EXISTS history (
+    historyId INTEGER DEFAULT nextval('history_seq') PRIMARY KEY NOT NULL,
+    transactionId VARCHAR(200) REFERENCES transaction(transactionId) NOT NULL,
+    accountId VARCHAR(200) REFERENCES account(accountId) NOT NULL,
+    balance NUMERIC
+);
+
+
