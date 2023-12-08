49bc0df9ece0f2858914be7401b97995e67a04f8
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-08 06:25:30 +0100

    fix : Update test of eccount to add test for balance and date the account and balance for account between star and end date

--- src/test/TestOfAccount.java
+++ src/test/TestOfAccount.java
@@ -3,6 +3,7 @@ package test;
 import model.Account;
 import dao.AccountCrudOperation;
 import model.Transaction;
+import service.HistoryOfBalance;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,20 +14,15 @@ public class TestOfAccount {
     public static void  TestAccount(){
         AccountCrudOperation accounts = new AccountCrudOperation();
 
-        //Variable :
+        // Variable using of test account :
 
         BigDecimal amount = new BigDecimal("100.45");
         BigDecimal balance1 = new BigDecimal("100.456");
         BigDecimal balance2 = new BigDecimal("200.456");
         BigDecimal balance3 = new BigDecimal("300.456");
         BigDecimal balance4 = new BigDecimal("400.456");
-
         LocalDateTime date = LocalDateTime.now();
-
-        // Find by id :
-        System.out.println("Find by Id account : ");
         List<Transaction> ListOfTransaction = new ArrayList<>();
-
         Transaction transaction1 = new Transaction("TXN004", amount, "Groceries", "CREDIT",date,"ACC001");
         Transaction transaction2 = new Transaction("TXN003", amount, "Dinner", "DEBIT",date,"ACC001");
         Transaction transaction3 = new Transaction("TXN003", amount, "Dinner", "CREDIT",date,"ACC002");
@@ -40,36 +36,44 @@ public class TestOfAccount {
         ListOfTransaction.add(transaction5);
         ListOfTransaction.add(transaction6);
 
-        Account account = new Account("ACC001", "John Doe", balance1,date,ListOfTransaction, "USD", "Bank");
+        //----------------------------------------Find by id------------------------------------ :
+        System.out.println("Find by Id account : ");
+        Account account = new Account("ACC002", "John Doe", balance2,date,ListOfTransaction, "USD", "Bank");
         accounts.findById(account);
 
-        //Save account :
+        //------------------------------------------Save account--------------------------------- :
         System.out.println("This account is saved  : ");
         Account Account5 = new Account("ACC005", "RKT Zo", balance2,date,ListOfTransaction, "USD", "Bank");
         accounts.save(Account5);
 
-        //save list of account :
+        //------------------------------------------save list of account----------------------------:
         System.out.println("This list of account is saved : ");
-        Account account5 = new Account("ACC005", "Rk Fabien", balance2,date,ListOfTransaction, "USD", "Bank");
-        Account setAccount6 = new Account("ACC006", "RKT Zo", balance3,date,ListOfTransaction, "EUR", "Cash");
+        Account setAccount5 = new Account("ACC005", "Rk Fabien", balance2,date,ListOfTransaction, "USD", "Bank");
+        Account account6 = new Account("ACC006", "RKT Zo", balance3,date,ListOfTransaction, "EUR", "Cash");
         List<Account> listAccount = new ArrayList<>();
-        listAccount.add(account5);
-        listAccount.add(setAccount6);
+        listAccount.add(setAccount5);
+        listAccount.add(account6);
         accounts.saveAll(listAccount);
 
-        // Update account :
+        //--------------------------------------------Update account------------------------------- :
         System.out.println("This account has been successfully updated :");
-        Account setAccount5 = new Account("ACC005", "RZ Fabien", balance4,date,ListOfTransaction, "GBP", "Cash");
-        accounts.update(setAccount5);
+        Account setAccount6 = new Account("ACC006", "Rakotonirina Zo", balance4,date,ListOfTransaction, "GBP", "Cash");
+        accounts.update(setAccount6);
 
-        // Find all account :
+        //--------------------------------------------Find all account------------------------------:
         System.out.println("List of account : ");
         accounts.findAll();
 
-        //List balance & date of transition ;
+        //-------------------------------------------List balance & date of transition--------------;
         System.out.println("Date of transition & balance of account : ");
         AccountCrudOperation.balanceAndDateOfAccount(account);
 
+        //--------------------------------------------List balance of account between start & end date-----------;
+        System.out.println("List balance of account between start & end date : ");
+        LocalDateTime start = LocalDateTime.of(2023, 12, 7, 10, 30);
+        LocalDateTime end = LocalDateTime.now();
+        Account account3 = new Account("ACC003", "Alice Johnson", balance3,date,ListOfTransaction, "GBP", "Mobile Money");
+        HistoryOfBalance.historyOfBalanceOfAccount(start,end,account3);
     }
 
 }
