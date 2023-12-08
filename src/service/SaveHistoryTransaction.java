fc723d181ddc2358e8af5cd3c714b3084d9a97e5
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-08 15:24:05 +0100

    feat : Create methode to save transaction of debtor and creditor

---
+++ src/service/SaveHistoryTransaction.java
@@ -0,0 +1,32 @@
+package service;
+
+import configuration.ConnectionDB;
+import model.Transaction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SaveHistoryTransaction {
+    private static Connection connection;
+    public static void getConnection() {
+        ConnectionDB Db = new ConnectionDB();
+        connection = Db.createConnection();
+    }
+
+    public static void registerTransactionHistory(Transaction creditor,Transaction debtor){
+        getConnection();
+        try{
+            String sql = "insert into TransferHistory (creditorTransactionId,debtorTransactionId,dateOfTransaction) values (?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1,creditor.getTransactionId());
+            statement.setString(2,debtor.getTransactionId());
+            statement.setObject(3,creditor.getDate());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
