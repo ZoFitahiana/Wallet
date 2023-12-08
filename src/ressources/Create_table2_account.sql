2e8f14bf4e100b55a6d2abaa585c49741f1e2882
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-08 17:54:04 +0100

    fix : Uptade insert of account

--- src/ressources/Create_table2_account.sql
+++ src/ressources/Create_table2_account.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS account (
 -- Insert data into account table
 INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, type)
 VALUES
-('ACC001', 'John Doe', 100.25, '2023-12-06 12:30:00', 'USD', 'Bank')
+('ACC001', 'John Doe', 100.25, '2023-12-06 12:30:00', 'EUR', 'Bank')
 ON CONFLICT (accountId) DO NOTHING;
 
 INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, type)
@@ -20,5 +20,5 @@ ON CONFLICT (accountId) DO NOTHING;
 
 INSERT INTO account (accountId, name, balance, lastUpdate, currencyId, type)
 VALUES
-('ACC003', 'Alice Johnson', 500.00, '2023-12-06 13:30:00', 'GBP', 'Mobile Money')
+('ACC003', 'Alice Johnson', 500.00, '2023-12-06 13:30:00', 'AR', 'Mobile Money')
 ON CONFLICT (accountId) DO NOTHING;
