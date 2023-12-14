c1d0cf8b0cd7a51ec85b0af728a8242abe6a015d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-14 19:34:08 +0100

    fix : Update table transaction

--- src/ressources/Create_table3_transaction.sql
+++ src/ressources/Create_table3_transaction.sql
@@ -5,21 +5,22 @@ CREATE TABLE IF NOT EXISTS transaction (
     label VARCHAR(255) NOT NULL,
     type VARCHAR(10) NOT NULL CHECK (type IN ('DEBIT', 'CREDIT')),
     date TIMESTAMP NOT NULL ,
-    accountId VARCHAR(255) REFERENCES account(accountId)
+    accountId VARCHAR(255) REFERENCES account(accountId),
+    categoriesId  VARCHAR(10) REFERENCES categories(categoriesId)
 );
 
 
 -- Insert  transaction
-INSERT INTO transaction  (transactionId, amount, label, type, date,accountId)
-VALUES ('TXN001', 100.50, 'Groceries', 'DEBIT', '2023-12-06 12:30:00','ACC001')
+INSERT INTO transaction  (transactionId, amount, label, type, date,accountId,categoriesId )
+VALUES ('TXN001', 100.50, 'Groceries', 'DEBIT', '2023-12-06 12:30:00','ACC001','INCOME')
 ON CONFLICT (transactionId) DO NOTHING;
 
 
-INSERT INTO transaction (transactionId, amount, label, type, date,accountId)
-VALUES ('TXN002', 50.25, 'Dinner', 'DEBIT', '2023-12-06 18:45:00','ACC002')
+INSERT INTO transaction (transactionId, amount, label, type, date,accountId,categoriesId )
+VALUES ('TXN002', 50.25, 'Dinner', 'DEBIT', '2023-12-06 18:45:00','ACC002','INCOME')
 ON CONFLICT (transactionId) DO NOTHING;
 
 
-INSERT INTO transaction (transactionId, amount, label, type, date,accountId)
-VALUES ('TXN003', 200.00, 'Salary', 'CREDIT', '2023-12-07 09:00:00','ACC003')
+INSERT INTO transaction (transactionId, amount, label, type, date,accountId,categoriesId )
+VALUES ('TXN003', 200.00, 'Salary', 'CREDIT', '2023-12-07 09:00:00','ACC003','EXPENSE')
 ON CONFLICT (transactionId) DO NOTHING;
