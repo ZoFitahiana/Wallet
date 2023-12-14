ef0703dc62ecfc19ed5dbe04e12aaa50b5e8a3f1
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-14 11:10:13 +0100

    fix : Update table transaction

--- src/ressources/Create_table3_transaction.sql
+++ src/ressources/Create_table3_transaction.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS transaction (
     transactionId VARCHAR(255) PRIMARY KEY,
     amount NUMERIC NOT NULL,
     label VARCHAR(255) NOT NULL,
-    type VARCHAR(10) NOT NULL CHECK (type IN ('DEBIT', 'CREDIT')),
+    categoriesId VARCHAR(200) REFERENCES categories(categoriesId),
     date TIMESTAMP NOT NULL ,
     accountId VARCHAR(255) REFERENCES account(accountId)
 );
@@ -11,15 +11,15 @@ CREATE TABLE IF NOT EXISTS transaction (
 
 -- Insert  transaction
 INSERT INTO transaction  (transactionId, amount, label, type, date,accountId)
-VALUES ('TXN001', 100.50, 'Groceries', 'DEBIT', '2023-12-06 12:30:00','ACC001')
+VALUES ('TXN001', 100.50, 'Groceries', 'CAT1', '2023-12-06 12:30:00','ACC001')
 ON CONFLICT (transactionId) DO NOTHING;
 
 
 INSERT INTO transaction (transactionId, amount, label, type, date,accountId)
-VALUES ('TXN002', 50.25, 'Dinner', 'DEBIT', '2023-12-06 18:45:00','ACC002')
+VALUES ('TXN002', 50.25, 'Dinner', 'CAT2', '2023-12-06 18:45:00','ACC002')
 ON CONFLICT (transactionId) DO NOTHING;
 
 
 INSERT INTO transaction (transactionId, amount, label, type, date,accountId)
-VALUES ('TXN003', 200.00, 'Salary', 'CREDIT', '2023-12-07 09:00:00','ACC003')
+VALUES ('TXN003', 200.00, 'Salary', 'CAT3', '2023-12-07 09:00:00','ACC003')
 ON CONFLICT (transactionId) DO NOTHING;
