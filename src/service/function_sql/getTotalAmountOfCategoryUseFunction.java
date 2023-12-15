83f29a12d96a1e11b087e9dc1d85ae345ef39fed
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2023-12-15 12:10:50 +0100

    feat : Create methode to map the function sql to generate Totale amount

---
+++ src/service/function_sql/getTotalAmountOfCategoryUseFunction.java
@@ -0,0 +1,36 @@
+package service.function_sql;
+
+import configuration.ConnectionDB;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class getTotalAmountOfCategoryUseFunction {
+        private static Connection connection;
+        public static void getConnection() {
+            ConnectionDB Db = new ConnectionDB();
+            connection = Db.createConnection();
+        }
+        public static void getCategoryNameAndAmountsUseFunction(String id , LocalDateTime startDate , LocalDateTime endDate){
+            getConnection();
+            try {
+                String sql = "SELECT * FROM getCategoryAmounts(?,?,?)";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1,id);
+                statement.setObject(2,startDate);
+                statement.setObject(3,endDate);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()){
+                    String category = resultSet.getString("category_name");
+                    BigDecimal TotalAmount = resultSet.getBigDecimal("total_amount");
+                    System.out.println("Category_name : " + category + " , TotalAmount :  " + TotalAmount );
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
